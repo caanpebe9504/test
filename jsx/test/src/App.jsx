fb7b6ca0e78a36621753b24b017a2dde81d0601c
Camilo Andrés Pérez Beltrán <cperezbe5545@gmail.com>
2024-09-29 12:56:39 -0500

    test_3

--- jsx/test/src/App.jsx
+++ jsx/test/src/App.jsx
@@ -1,93 +1,25 @@
-import { useState , useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import React from 'react'
+import Title from "./components/title"
 
 
-function App() {
+async function App() {
 
-  const [ currentArtistInfo, setCurrentArtistInfo  ] = useState(null);
+  const url = "https://randomuser.me/api/"
 
-  const randomArtistInfo = async() => {
-
-     try{
-      const urlResponse = await fetch("https://randomuser.me/api/")
-
-      if(!urlResponse.ok) {
-        throw new Error ("Se jodió")
-      }
-      const artistInfo = await urlResponse.json();
-      console.log("Fetched data:", artistInfo)
-      setCurrentArtistInfo (artistInfo.results[0])
-     } catch (error) {
-      console.log("Error fetching data:", error)
-     }
-  
-  }
-
-  useEffect(() => {
-    randomArtistInfo()
-  }, []);
+  const fetchResult = await fetch(url)
+  const dataFetched = await fetchResult.json()
 
   return (
-    <div>
-
-      <h1>Artista/personaje Favorito</h1>
-      {currentArtistInfo ? (
-        <>
-
-        <img src = {currentArtistInfo.picture.large} alt = "Artist Picture" />
-
-        <p>
-          Nombre: {currentArtistInfo.name.title} {currentArtistInfo.name.first} {currentArtistInfo.name.last}<br />
-          Género: {currentArtistInfo.gender}<br />
-          Pais de origen: {currentArtistInfo.location.country}<br />
-        </p>
-
-      </>
-    ) : (
     
-      <p>La data del artista está en ejecución</p>
-    )}
-    <button onClick ={randomArtistInfo  }> Click acá para ver otro artista </button>
+    <div>
+      {/* Titulo de el artista/personaje */}
+      <Titulo
+        title={dataFetched.results[0].name.title}
+      />
     </div>
-  );
-}
-
-export default App;
-
-
-/*
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
-
-*/
+export default App;
---
+++ jsx/test/src/components/title.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+// const url = "https://randomuser.me/api/"
+
+function Title(props){
+
+    return (
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    )
+    
+
+}
+
+export default Title;
