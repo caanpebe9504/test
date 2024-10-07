f450df3b3e1e122dd615e71a7e4b19ac19777587
Camilo Andrés Pérez Beltrán <cperezbe5545@gmail.com>
2024-10-07 13:01:19 -0500

    FizzBuzz

--- Junior_test/test.js
+++ Junior_test/test.js
@@ -1,13 +1,13 @@
 
-function fizzBuzz(){
+const fizBuzz = () => {
 
-    for (let i = 1; i <= 100; i++) {
-
-        if (i % 3 === 0 && i % 5 === 0) {
+    for(let i = 1; i < 100; i++){
+        
+        if(i % 3 === 0 && i % 5 === 0){
             console.log("FizzBuzz")
-        } else if (i % 3 === 0){
+        }else if(i % 3 === 0){
             console.log("Fizz")
-        }else if(i % 5 === 0){
+        }else if (i % 5 === 0){
             console.log("Buzz")
         } else {
             console.log(i)
@@ -15,4 +15,4 @@ function fizzBuzz(){
     }
 }
 
-fizzBuzz()
+fizBuzz()
